6ca74445e7229fb590b6c58b68c871b0086598d3
Asumu Takikawa <asumu@interchange.ubc.ca>
2010-08-23 22:37:50 -0400

    Swap expected and actual positions. Replace newlines with Util.NL for cross-platform testing. Added a test case for Windows paths.

--- plugin/src/checkers/eclipse/javac/tests/ErrorParserTest.java
+++ plugin/src/checkers/eclipse/javac/tests/ErrorParserTest.java
@@ -7,64 +7,97 @@ import java.util.List;
 import org.junit.Test;
 
 import checkers.eclipse.javac.JavacError;
+import checkers.eclipse.util.Util;
 
 public class ErrorParserTest
 {
     private static final String SIMPLE_TEST_INPUT = new StringBuilder()
-            .append("/home/asumu/gsoc-workspace-4/checker testing/src/GetStarted.java:8: warning: incompatible types.\n")
-            .append("       @NonNull Integer bar = null;\n")
-            .append("                              ^\n")
-            .append("  found   : null\n")
-            .append("  required: @NonNull Integer\n")
-            .append("/home/asumu/gsoc-workspace-4/checker testing/src/GetStarted.java:16: warning: attempting to use a non-@Interned comparison operand\n")
-            .append("       else if (s1 == obj)\n")
-            .append("                      ^\n").append("  found: Object\n")
-            .append("2 warnings").toString();
+            .append("/home/asumu/gsoc-workspace-4/checker testing/src/GetStarted.java:8: warning: incompatible types.")
+            .append(Util.NL)
+            .append("       @NonNull Integer bar = null;")
+            .append(Util.NL)
+            .append("                              ^")
+            .append(Util.NL)
+            .append("  found   : null")
+            .append(Util.NL)
+            .append("  required: @NonNull Integer")
+            .append(Util.NL)
+            .append("/home/asumu/gsoc-workspace-4/checker testing/src/GetStarted.java:16: warning: attempting to use a non-@Interned comparison operand")
+            .append(Util.NL).append("       else if (s1 == obj)")
+            .append(Util.NL).append("                      ^").append(Util.NL)
+            .append("  found: Object").append(Util.NL).append("2 warnings")
+            .toString();
     private static final String SIMPLE_ERROR_1 = new StringBuilder()
-            .append("incompatible types.\n")
-            .append("       @NonNull Integer bar = null;\n")
-            .append("  found   : null\n")
-            .append("  required: @NonNull Integer\n").toString();
+            .append("incompatible types.").append(Util.NL)
+            .append("       @NonNull Integer bar = null;").append(Util.NL)
+            .append("  found   : null").append(Util.NL)
+            .append("  required: @NonNull Integer").toString();
     private static final String SIMPLE_ERROR_2 = new StringBuilder()
-            .append("attempting to use a non-@Interned comparison operand\n")
-            .append("       else if (s1 == obj)\n").append("  found: Object\n")
-            .toString();
+            .append("attempting to use a non-@Interned comparison operand")
+            .append(Util.NL).append("       else if (s1 == obj)")
+            .append(Util.NL).append("  found: Object").toString();
     private static final String OTHER_TEST_INPUT = new StringBuilder()
-            .append("/homes/gws/wmdietl/research/eclipse-workspaces/2010-08-icse/SwingEval/AwtSwing/java/awt/Window.java:58: warning: Disposer is internal proprietary API and may be removed in a future release\n")
-            .append("import sun.java2d.Disposer;\n")
-            .append("                 ^\n")
-            .append("/homes/gws/wmdietl/research/eclipse-workspaces/2010-08-icse/SwingEval/AwtSwing/java/awt/Window.java:59: warning: Region is internal proprietary API and may be removed in a future release\n")
-            .append("import sun.java2d.pipe.Region;\n")
-            .append("                      ^\n").append("2 warnings")
-            .toString();
+            .append("/homes/gws/wmdietl/research/eclipse-workspaces/2010-08-icse/SwingEval/AwtSwing/java/awt/Window.java:58: warning: Disposer is internal proprietary API and may be removed in a future release")
+            .append(Util.NL)
+            .append("import sun.java2d.Disposer;")
+            .append(Util.NL)
+            .append("                 ^")
+            .append(Util.NL)
+            .append("/homes/gws/wmdietl/research/eclipse-workspaces/2010-08-icse/SwingEval/AwtSwing/java/awt/Window.java:59: warning: Region is internal proprietary API and may be removed in a future release")
+            .append(Util.NL).append("import sun.java2d.pipe.Region;")
+            .append(Util.NL).append("                      ^").append(Util.NL)
+            .append("2 warnings").toString();
     private static final String OTHER_ERROR_1 = new StringBuilder()
-            .append("Disposer is internal proprietary API and may be removed in a future release\n")
-            .append("import sun.java2d.Disposer;\n").toString();
+            .append("Disposer is internal proprietary API and may be removed in a future release")
+            .append(Util.NL).append("import sun.java2d.Disposer;").toString();
     private static final String OTHER_ERROR_2 = new StringBuilder()
-            .append("Region is internal proprietary API and may be removed in a future release\n")
-            .append("import sun.java2d.pipe.Region;\n").toString();
-    private static final String W_TEST_INPUT = new StringBuilder()
-            .append("/foo/bar/Baz.java:35: warning: AttributesValues is internal proprietary API and may be removed in a future release\n")
-            .append("  private static void applyStyle(int style, AttributeValues values) {\n")
-            .append("/foo/bar/Baz.java:38: package sun.java2d.cmm does not exist\n")
-            .append("import sun.java2d.cmm.ColorTransform\n")
-            .append("/foo/bar/Baz.java:39: package sun.java2d.cmm does not exist\n")
-            .append("import sun.java2d.cmm.CMSManager\n").append("3 warnings")
+            .append("Region is internal proprietary API and may be removed in a future release")
+            .append(Util.NL).append("import sun.java2d.pipe.Region;")
             .toString();
+    private static final String W_TEST_INPUT = new StringBuilder()
+            .append("/foo/bar/Baz.java:35: warning: AttributesValues is internal proprietary API and may be removed in a future release")
+            .append(Util.NL)
+            .append("  private static void applyStyle(int style, AttributeValues values) {")
+            .append(Util.NL)
+            .append("/foo/bar/Baz.java:38: package sun.java2d.cmm does not exist")
+            .append(Util.NL)
+            .append("import sun.java2d.cmm.ColorTransform")
+            .append(Util.NL)
+            .append("/foo/bar/Baz.java:39: package sun.java2d.cmm does not exist")
+            .append(Util.NL).append("import sun.java2d.cmm.CMSManager")
+            .append(Util.NL).append("3 warnings").toString();
     private static final String W_ERROR_2 = new StringBuilder()
-            .append("package sun.java2d.cmm does not exist\n")
-            .append("import sun.java2d.cmm.ColorTransform\n").toString();
+            .append("package sun.java2d.cmm does not exist").append(Util.NL)
+            .append("import sun.java2d.cmm.ColorTransform").toString();
+    private static final String WINDOWS_INPUT = new StringBuilder()
+            .append("C:/workspace/checker testing/src/GetStarted.java:8: warning: incompatible types.")
+            .append(Util.NL)
+            .append("       @NonNull Integer bar = null;")
+            .append(Util.NL)
+            .append("                              ^")
+            .append(Util.NL)
+            .append("  found   : null")
+            .append(Util.NL)
+            .append("  required: @NonNull Integer")
+            .append(Util.NL)
+            .append("C:/workspace/checker testing/src/GetStarted.java:16: warning: attempting to use a non-@Interned comparison operand")
+            .append(Util.NL).append("       else if (s1 == obj)")
+            .append(Util.NL).append("                      ^").append(Util.NL)
+            .append("  found: Object").append(Util.NL).append("2 warnings")
+            .toString();
+    private static final String WINDOWS_ERROR_MSG_1 = SIMPLE_ERROR_1;
+    private static final String WINDOWS_ERROR_MSG_2 = SIMPLE_ERROR_2;
 
     @Test
     public void simpleParseTest()
     {
         List<JavacError> errors = JavacError.parse(SIMPLE_TEST_INPUT);
 
-        assertEquals(errors.size(), 2);
-        assertEquals(errors.get(0).lineNumber, 8);
-        assertEquals(errors.get(0).message, SIMPLE_ERROR_1);
-        assertEquals(errors.get(1).lineNumber, 16);
-        assertEquals(errors.get(1).message, SIMPLE_ERROR_2);
+        assertEquals(2, errors.size());
+        assertEquals(8, errors.get(0).lineNumber);
+        assertEquals(SIMPLE_ERROR_1, errors.get(0).message);
+        assertEquals(16, errors.get(1).lineNumber);
+        assertEquals(SIMPLE_ERROR_2, errors.get(1).message);
     }
 
     @Test
@@ -72,11 +105,11 @@ public class ErrorParserTest
     {
         List<JavacError> errors = JavacError.parse(OTHER_TEST_INPUT);
 
-        assertEquals(errors.size(), 2);
-        assertEquals(errors.get(0).message, OTHER_ERROR_1);
-        assertEquals(errors.get(0).lineNumber, 58);
-        assertEquals(errors.get(1).message, OTHER_ERROR_2);
-        assertEquals(errors.get(1).lineNumber, 59);
+        assertEquals(2, errors.size());
+        assertEquals(OTHER_ERROR_1, errors.get(0).message);
+        assertEquals(58, errors.get(0).lineNumber);
+        assertEquals(OTHER_ERROR_2, errors.get(1).message);
+        assertEquals(59, errors.get(1).lineNumber);
     }
 
     @Test
@@ -84,7 +117,19 @@ public class ErrorParserTest
     {
         List<JavacError> errors = JavacError.parse(W_TEST_INPUT);
 
-        assertEquals(errors.size(), 3);
-        assertEquals(errors.get(1).message, W_ERROR_2);
+        assertEquals(3, errors.size());
+        assertEquals(W_ERROR_2, errors.get(1).message);
+    }
+
+    @Test
+    public void windowsParseTest()
+    {
+        List<JavacError> errors = JavacError.parse(WINDOWS_INPUT);
+
+        assertEquals(2, errors.size());
+        assertEquals(8, errors.get(0).lineNumber);
+        assertEquals(WINDOWS_ERROR_MSG_1, errors.get(0).message);
+        assertEquals(16, errors.get(1).lineNumber);
+        assertEquals(WINDOWS_ERROR_MSG_2, errors.get(1).message);
     }
 }
