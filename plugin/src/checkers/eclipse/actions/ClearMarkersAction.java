f98a2bdcbdaf178af06b3c32dd70fd0bbd6f2192
Asumu Takikawa <asumu@interchange.ubc.ca>
2010-08-24 01:08:01 -0400

    Overhaul menus to the new org.eclipse.ui.menus API

--- plugin/src/checkers/eclipse/actions/ClearMarkersAction.java
+++ plugin/src/checkers/eclipse/actions/ClearMarkersAction.java
@@ -1,74 +1,79 @@
 package checkers.eclipse.actions;
 
-import static checkers.eclipse.util.JavaUtils.*;
+import static checkers.eclipse.util.JavaUtils.iterable;
 
-import java.lang.reflect.*;
-import java.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.*;
-import org.eclipse.jface.operation.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.ui.*;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-import checkers.eclipse.*;
-import checkers.eclipse.util.*;
+import checkers.eclipse.CheckerPlugin;
+import checkers.eclipse.util.MarkerUtil;
 
 /**
  * Remove all bug markers for the currently selected project.
  */
-public class ClearMarkersAction implements IObjectActionDelegate {
-
+public class ClearMarkersAction extends CheckerHandler
+{
     /** The current selection. */
     private IStructuredSelection currSelection;
     private final MarkerCleaner cleaner = new MarkerCleaner();
-
-    @Override
-    public final void setActivePart(final IAction action,
-            final IWorkbenchPart targetPart) {
-        // noop
-    }
-
-    @Override
-    public final void selectionChanged(final IAction action,
-            final ISelection selection) {
-        if (selection instanceof IStructuredSelection)
-            currSelection = (IStructuredSelection) selection;
-        else
-            currSelection = null;
-    }
 
     /**
      * Clear the markers on each project in the given selection, displaying a
      * progress monitor.
      * 
      */
-    @Override
-    public final void run(final IAction action) {
-        if (currSelection == null || currSelection.isEmpty())
-            return;
+    public Object execute(ExecutionEvent event)
+    {
+        ISelection selection = HandlerUtil.getActiveMenuSelection(event);
 
-        try {
+        if (selection instanceof IStructuredSelection)
+            currSelection = (IStructuredSelection) selection;
+        else
+            currSelection = null;
+
+        if (currSelection == null || currSelection.isEmpty())
+            return null;
+
+        try
+        {
             ProgressMonitorDialog progress = new ProgressMonitorDialog(
                     CheckerPlugin.getShell());
             progress.run(true, true, cleaner);
-        } catch (InvocationTargetException e) {
+        }catch (InvocationTargetException e)
+        {
             CheckerPlugin.logException(e,
                     "InvocationTargetException on clear markers");
-        } catch (InterruptedException e) {
-            CheckerPlugin.logException(e, "InterruptedException on clear markers");
+        }catch (InterruptedException e)
+        {
+            CheckerPlugin.logException(e,
+                    "InterruptedException on clear markers");
         }
+
+        return null;
     }
 
-    private class MarkerCleaner implements IRunnableWithProgress {
+    private class MarkerCleaner implements IRunnableWithProgress
+    {
         @Override
-        public void run(IProgressMonitor pm) throws InvocationTargetException {
-            try {
+        public void run(IProgressMonitor pm) throws InvocationTargetException
+        {
+            try
+            {
                 @SuppressWarnings("unchecked")
                 Iterator<IAdaptable> iter = currSelection.iterator();
-                for (IAdaptable adaptable : iterable(iter)) {
+                for (IAdaptable adaptable : iterable(iter))
+                {
                     IResource resource = (IResource) adaptable
                             .getAdapter(IResource.class);
 
@@ -79,8 +84,10 @@ public class ClearMarkersAction implements IObjectActionDelegate {
                             + resource.getName());
                     MarkerUtil.removeMarkers(resource);
                 }
-            } catch (CoreException ex) {
-                CheckerPlugin.logException(ex, "CoreException on clear markers");
+            }catch (CoreException ex)
+            {
+                CheckerPlugin
+                        .logException(ex, "CoreException on clear markers");
                 throw new InvocationTargetException(ex);
             }
         }
--- plugin/src/checkers/eclipse/actions/ConvertProjectAction.java
+++ plugin/src/checkers/eclipse/actions/ConvertProjectAction.java
@@ -1,44 +1,47 @@
 package checkers.eclipse.actions;
 
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import checkers.eclipse.CheckerPlugin;
 import checkers.eclipse.natures.CheckerBuildNature;
 
-public class ConvertProjectAction implements IActionDelegate
+public class ConvertProjectAction extends CheckerHandler
 {
-
-    private IStructuredSelection selection;
-
-    public ConvertProjectAction()
+    public Object execute(ExecutionEvent event) throws ExecutionException
     {
-        this.selection = null;
-    }
+        ISelection selection = HandlerUtil.getActiveMenuSelection(event);
+        IJavaElement element = element(selection);
+        Command command = event.getCommand();
+        boolean state = HandlerUtil.toggleCommandState(command);
 
-    @Override
-    public void run(IAction action)
-    {
         try
         {
-            IProject project = element().getJavaProject().getProject();
+            IProject project = element.getJavaProject().getProject();
             IProjectDescription desc = project.getDescription();
             String[] natures = desc.getNatureIds();
+            boolean hasNature = hasNature(natures);
 
-            if (!hasNature(natures))
-                setNature(project, desc, natures);
-            else
-                removeNature(project, desc, natures);
+            if (hasNature == state)
+            {
+                if (!hasNature)
+                    setNature(project, desc, natures);
+                else
+                    removeNature(project, desc, natures);
+            }
         }catch (CoreException e)
         {
             CheckerPlugin.logException(e, e.getMessage());
         }
+
+        return null;
     }
 
     private boolean hasNature(String[] natures)
@@ -88,26 +91,4 @@ public class ConvertProjectAction implements IActionDelegate
 
         project.setDescription(desc, null);
     }
-
-    @Override
-    public void selectionChanged(IAction action, ISelection selection)
-    {
-        if (!selection.isEmpty() && selection instanceof IStructuredSelection)
-            this.selection = (IStructuredSelection) selection;
-        else
-            this.selection = null;
-    }
-
-    private IJavaElement element()
-    {
-        Object element = selection.getFirstElement();
-
-        if (selection != null && element instanceof IJavaElement)
-        {
-            return (IJavaElement) element;
-        }
-
-        return null;
-    }
-
 }
